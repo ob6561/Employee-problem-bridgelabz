9dad296a49c095cd91e3636874ba9a93c01975a9
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-08 15:05:13 +0530

    Implement Use Case 1 - <short description>

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int IS_FULL_TIME = 1;
+            Random rd = new Random();
+            int empcheck = rd.Next(0, 2);
+            if(empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
